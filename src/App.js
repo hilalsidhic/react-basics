9b819c165cebb86601712d411fb544304133d829
hilalsidhic <hilalsidhic21@gmail.com>
2021-09-22 10:14:26 +0530

    useState commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css'
 import Header from './components/Header.js'
+import { useState } from 'react'
 function App() {
   const data="hello hilal"
+  const [count,setCount]=useState(0)
+  let addcounter=()=>{
+    setCount(count+1)
+    console.log(count)
+  }
   return (
     <div>
       <Header data={data}/>
       <p>this is description {data}</p>
-    </div>
-    
+      <h2>Counter : {count}</h2>
+      <button onClick={addcounter}>click Me</button>
+    </div> 
   );
 }
 
