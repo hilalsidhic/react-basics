bce18c47f8f3e0ddf0f6a71f0ea2482df52fca74
hilalsidhic <hilalsidhic21@gmail.com>
2021-09-27 01:42:52 +0530

    useEffect completed

--- src/Newcounter.jsx
+++ src/Newcounter.jsx
@@ -1,11 +1,23 @@
-import React,{ useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 
 function Newcounter() {
-    const [count,setCount]=useState(0)
+    const [count1,setCount1]=useState(0)
+    const [count2,setCount2]=useState(0)
+    useEffect(()=>{
+        console.log('mounting')
+        console.log('updating....'+count1);
+        console.log('updating....'+count2);
+        return()=>{
+            console.log('cleanup ..'+count1)
+            console.log('cleanup....'+count2);
+        }
+    },[count1,count2]);   
     return (
         <div>
-            <button onClick={()=>setCount(count+1)}>click me</button>
-            <h1>This is a new counter : {count}</h1>
+            <button onClick={()=>setCount1(count1+1)}>click me</button>
+            <h1>This is a new counter : {count1}</h1>
+            <button onClick={()=>setCount2(count2+1)}>click me</button>
+            <h1>This is a new counter : {count2}</h1>
         </div>
     )
 }
