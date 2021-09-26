6bd39b1350ca980cd0d844e809666de653f8f4bb
hilalsidhic <hilalsidhic21@gmail.com>
2021-09-26 19:00:13 +0530

    external components used for hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
 import Header from './components/Header.js'
 import { useState } from 'react'
+import Counter from './Counter'
 function App() {
   const data="hello hilal"
-  const [count,setCount]=useState(0)
+  const [count,setCount]=useState(1)
   let addcounter=()=>{
     setCount(count+1)
     console.log(count)
@@ -12,7 +13,7 @@ function App() {
     <div>
       <Header data={data}/>
       <p>this is description {data}</p>
-      <h2>Counter : {count}</h2>
+      <Counter count={count} />
       <button onClick={addcounter}>click Me</button>
     </div> 
   );
---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ let Counter = (props)=> {
+    return (
+        <div>
+            <h2>Counter : {props.count}</h2>
+        </div>
+    )
+}
+
+export default Counter
